f5abc19e905452f2ca111caf382603e4e8fc83b6
KunalWadhai <kunalwadhai127@gmail.com>
2025-11-23 19:39:11 +0530

    feat: minimal security through helmet, response compression & api response logs

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,13 @@ import messageRoutes from './routes/message.route.js';
 import {connectDB} from './lib/db.js';
 import {ENV} from './lib/env.js'
 
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+import rateLimit from 'express-rate-limit';
+
 import dotenv from 'dotenv';
+import { uptime } from 'process';
 dotenv.config(); 
 
 export const app = express(); 
@@ -20,9 +26,40 @@ app.use(express.json()); // for accessing req.body content that sent by user (fi
 app.use(express.urlencoded({extended:true})); 
 app.use(cookieParser()); // to parse the cookies.
 
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+
+// -----------Rate Limiter
+const limiter = new rateLimit({
+   windowMs: 15 * 60 * 1000,
+   max:100,
+   statusCode:429,
+   message: "Too many requests from this IP, Please try after some time"
+});
+
+app.use("/api", limiter);
+
 //---------Routes--------------
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Health Check
+app.get("/health", (req, res) => {
+   res.status(200).json({
+      status:'OK',
+      Timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+   });
+});
+
+// 404 Handler
+app.get("*", (req,res) => {
+   res.status(404).json({
+      success: false,
+      message:"Router Not Found"
+   })
+});
 
 // -- Make ready for the deployment
 if(ENV.NODE_ENV === "production"){
