0957e1d42c4c2a90332e9b9e1bedba904396c6bd
KunalWadhai <kunalwadhai127@gmail.com>
2026-02-06 11:58:08 +0530

    refactor: db connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,8 +4,8 @@ import {ENV} from './constants.js'
 export const connectDB = async () => {
     try{
         const {MONGODB_URI} = ENV;
-        if(!ENV.MONGODB_URI) throw new Error("MONGODB URI in not defined");
-        const conn = await mongoose.connect(ENV.MONGODB_URI);
+        if(!MONGODB_URI) throw new Error("MONGODB URI in not defined");
+        const conn = await mongoose.connect(MONGODB_URI);
         console.log("MONGODB Connected Successfully: ", conn.connection.host);
     }catch(error){
         console.log("Database Connection Failed",error);
